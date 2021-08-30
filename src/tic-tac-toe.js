74f75efa6310d8d6ec13ec4f8713d934b47d3e7f
604karev <604karev@gmail.com>
2021-08-30 20:11:58 +0300

    feat: add solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,69 @@
 class TicTacToe {
-    constructor() {
 
+    constructor() {
+        this.field = [[null, null, null], [null, null, null], [null, null, null]];
+        this.currentSymbol = 'x';
+        this.winner = null;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.field[rowIndex][columnIndex]) {
+            return;
+        }
 
+        this.field[rowIndex][columnIndex] = this.currentSymbol;
+        this.currentSymbol = this.currentSymbol === 'x' ? 'o' : 'x';
+
+        let isGameOver = false;
+        let symbol;
+
+        for (let i = 0; i < 3; i++) {
+            symbol = this.field[i][0];
+            isGameOver = this.field[i].every(one => one === symbol);
+            if (isGameOver) break;
+
+            symbol = this.field[0][i];
+            isGameOver = this.field.every(arr => arr[i] === symbol);
+            if (isGameOver) break;
+        }
+        if (!isGameOver) {
+            symbol = this.field[1][1];
+            isGameOver = this.field.every((arr, i) => arr[i] === symbol)
+                || this.field.every((arr, i) => arr[2 - i] === symbol);
+        }
+
+        if (isGameOver) {
+            this.winner = symbol;
+            return;
+        }
+
+        if (this.noMoreTurns()) {
+            this.winner = 'draw';
+        }
     }
 
     isFinished() {
-
+        return !!this.winner;
     }
 
     getWinner() {
-
+        return this.winner === 'draw' ? null : this.winner;
     }
 
     noMoreTurns() {
-
+        return this.field.every(arr => arr.every(one => one !== null));
     }
 
     isDraw() {
-
+        return this.winner === 'draw';
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
